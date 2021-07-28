6ad5c7cc32d639334895b35d33c57dbc6f9f52f7
Raghav Sharma <raghav_tinker@yahoo.com>
2021-07-28 16:25:59 +0530

    Flash chat app done

--- Flash-Chat/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash-Chat/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,10 +14,46 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    let db = Firestore.firestore()
+    var messages: [Message] = [
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
         title = K.appName
         navigationItem.hidesBackButton = true
+        
+        tableView.dataSource = self
+        //tableView.delegate = self
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
+        
+        loadMessages()
+    }
+    
+    func loadMessages(){
+        db.collection(K.FStore.collectionName).order(by: K.FStore.dateField).addSnapshotListener { (querySnapshot, error) in
+            self.messages = []
+            if let e = error{
+                print("Issue in reading \(e)")
+            }
+            else{
+                if let snapshotDocuments = querySnapshot?.documents{
+                    for snap in snapshotDocuments{
+                        print(snap.data())
+                        let data = snap.data()
+                        if let sender = data[K.FStore.senderField] as? String, let messageBody = data[K.FStore.bodyField] as? String{
+                            self.messages.append(Message(sender: sender, body: messageBody))
+                            
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                                let indexPath =
+                                    IndexPath(row: self.messages.count - 1, section: 0)
+                                self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
     
     @IBAction func logOutPressed(_ sender: Any) {
@@ -30,5 +66,54 @@ class ChatViewController: UIViewController {
         }
     }
     @IBAction func sendPressed(_ sender: UIButton) {
+        if let messageBody: String = messageTextfield.text, let messageSender: String = Auth.auth().currentUser?.email{
+            // All is valid
+            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField: messageSender, K.FStore.bodyField: messageBody, K.FStore.dateField: Date().timeIntervalSince1970]) { (error) in
+                if let e = error{
+                    print("Some error \(e)")
+                }
+                else{
+                    print("Success in saving data")
+                    self.messageTextfield.text = ""
+                    self.messageTextfield.placeholder = "Type your message"
+                }
+            }
+        }
     }
 }
+
+
+extension ChatViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let message = messages[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReusableCell", for: indexPath) as! MessageCell
+        cell.label.text = message.body
+
+        if message.sender == Auth.auth().currentUser?.email{
+            cell.leftImageView.isHidden = true
+            cell.rightImageView.isHidden = false
+            cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.lightPurple)
+            cell.label.textColor = UIColor(named: K.BrandColors.purple)
+        }
+        else{
+            cell.leftImageView.isHidden = false
+            cell.rightImageView.isHidden = true
+            cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.purple)
+            cell.label.textColor = UIColor(named: K.BrandColors.lightPurple)
+        }
+        return cell
+    }
+}
+
+/*
+extension ChatViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+}
+*/
